f2965185a4870b502d3bfc948566189bdd408afc
maksym davydenko <maksim.davydenko.work@gmail.com>
2025-03-03 23:10:14 +0200

    fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import OrdersPage from "../src/pages/OrdersPage";
 import OrderDetailPage from "../src/pages/OrderDetailPage";
 import "../src/index.css";
